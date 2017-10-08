2ce4ca350e9a0e1c1d1be80b6545d2b55f1792d0
Alexander Yaroshevich <vozman@yandex.ru>
2017-10-08 16:59:35 +0300

    refactor image movement

--- UI/image_helper.py
+++ UI/image_helper.py
@@ -21,26 +21,24 @@ class ImageHelper:
         self.image = self.openslide_image.read_region(self.current_coordinates, self.current_level,
                                                       self.level_dimensions[self.current_level])
         self.image_slide = openslide.ImageSlide(self.image)
-        print self.image_slide.level_downsamples
-        print self.current_level
-        print self.level_dimensions
+        print self.image_dimensions
+        self.print_status()
 
     def __calculate_movement_step_coordinates(self):
         self.current_movement_step = (self.image_dimensions[0] / self.scale_factor,
                                       self.image_dimensions[1] / self.scale_factor)
+        print 'Current movement step:', self.current_movement_step
 
     def get_q_image(self):
 
         self.image = self.openslide_image.read_region(self.current_coordinates, self.current_level,
                                                       self.current_window_size)
-        print self.current_coordinates, self.current_level, self.current_window_size
+        self.print_status()
         return ImageQt.ImageQt(self.image)
 
-    def change_image_properties(self, coordinates, level, size):
-        self.current_coordinates = coordinates
-        self.current_level = level
-        self.current_window_size = size
-        self.image = self.openslide_image.read_region(coordinates, self.current_level, size)
+    def change_image_properties(self):
+        self.image = self.openslide_image.read_region(self.current_coordinates,
+                                                      self.current_level, self.current_window_size)
         return ImageQt.ImageQt(self.image)
 
     # Zooming is just moving to next level of image
@@ -49,46 +47,52 @@ class ImageHelper:
             self.current_level = self.current_level - 1
             self.scale_factor *= SCALE_MULTIPLIER
             self.__calculate_movement_step_coordinates()
-        return self.change_image_properties(self.current_coordinates, self.current_level,
-                                            self.current_window_size)
+        return self.change_image_properties()
 
     def zoom_out(self):
         if self.current_level < self.openslide_image.level_count - 1:
             self.current_level = self.current_level + 1
             self.scale_factor /= SCALE_MULTIPLIER
             self.__calculate_movement_step_coordinates()
-        return self.change_image_properties(self.current_coordinates, self.current_level,
-                                            self.current_window_size)
+        return self.change_image_properties()
 
     # TODO look at openslide library http://openslide.org/api/python/#module-openslide
     def move_right(self):
-        if (self.current_coordinates[0] + SCALE_MULTIPLIER * self.current_movement_step[0]) <= \
-                self.image_dimensions[0]:
-            self.current_coordinates = (self.current_coordinates[0] + self.current_movement_step[0],
-                                        self.current_coordinates[1])
-            print self.level_dimensions[self.current_level]
-            print self.current_coordinates
+        self.set_coordinates(self.current_coordinates[0] + self.current_movement_step[0],
+                             self.current_coordinates[1])
 
     def move_left(self):
-        if self.current_coordinates[0] - self.current_movement_step[0] >= 0:
-            self.current_coordinates = (self.current_coordinates[0] - self.current_movement_step[0],
-                                        self.current_coordinates[1])
-            print self.level_dimensions[self.current_level]
-            print self.current_coordinates
+        self.set_coordinates(self.current_coordinates[0] - self.current_movement_step[0],
+                             self.current_coordinates[1])
 
     def move_down(self):
-        if (self.current_coordinates[1] + SCALE_MULTIPLIER * self.current_movement_step[1]) <= \
-                self.image_dimensions[1]:
-            self.current_coordinates = (
-                self.current_coordinates[0],
-                self.current_coordinates[1] + self.current_movement_step[1])
-            print self.level_dimensions[self.current_level]
-            print self.current_coordinates
+        self.set_coordinates(self.current_coordinates[0],
+                             self.current_coordinates[1] + self.current_movement_step[1])
 
     def move_up(self):
-        if self.current_coordinates[1] - self.current_movement_step[1] >= 0:
-            self.current_coordinates = (
-                self.current_coordinates[0],
-                self.current_coordinates[1] - self.current_movement_step[1])
-            print self.level_dimensions[self.current_level]
-            print self.current_coordinates
+        self.set_coordinates(self.current_coordinates[0],
+                             self.current_coordinates[1] - self.current_movement_step[1])
+
+    def set_coordinates(self, x, y):
+        to_set_x = self.__get_correct_coordinate(x, self.image_dimensions[0],
+                                                 self.current_movement_step[1])
+        to_set_y = self.__get_correct_coordinate(y, self.image_dimensions[1],
+                                                 self.current_movement_step[1])
+        self.current_coordinates = (to_set_x, to_set_y)
+        self.print_status()
+
+    @staticmethod
+    def __get_correct_coordinate(coordinate, dimension, step):
+        if coordinate > 0:
+            if coordinate < dimension:
+                return coordinate
+            else:
+                return dimension - step
+        else:
+            return 0
+
+    def print_status(self):
+        print 'Current level:', self.current_level
+        print 'Level dimensions:', self.level_dimensions[self.current_level]
+        print 'Coordinates:', self.current_coordinates
+        print '\n'
