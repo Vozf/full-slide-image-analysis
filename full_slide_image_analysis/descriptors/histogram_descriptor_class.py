6244692933d983dab3339b5a5973b6e3a614d21f
Dmitry Voynov <cry_miss@mail.ru>
2017-10-11 01:21:07 +0300

    started histogram descriptor

---
+++ full_slide_image_analysis/descriptors/histogram_descriptor_class.py
@@ -0,0 +1,18 @@
+from full_slide_image_analysis.descriptors.descriptor_class import Descriptor
+import numpy
+
+
+class HistogramDescriptor(Descriptor):
+    def __init__(self, tile, scheme):
+        Descriptor.__init__(self, tile)
+        self.scheme = scheme
+        (self.r_mod, self.g_mod, self.b_mod) = self.scheme
+
+    def rgb_use_scheme(self, rgb):
+        (r, g, b) = rgb
+        res = r >> (8 - self.r_mod) << (8 - self.r_mod)\
+                    + g >> (8 - self.g_mod) << (8 - self.g_mod - self.r_mod)\
+                    + b >> (8 - self.b_mod)
+
+    def _calc(self):
+        tile = self.tile
