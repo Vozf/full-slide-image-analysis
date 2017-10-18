4287f0c62e443d5e5ec24621e274b03bd4f8f268
Alexander Yaroshevich <vozman@yandex.ru>
2017-10-18 16:13:59 +0300

    add generic parameters descriptor initialization

--- slide_analysis/UI/model/model_class.py
+++ slide_analysis/UI/model/model_class.py
@@ -3,13 +3,15 @@ from slide_analysis.descriptors import TestDescriptor
 from slide_analysis.initialize_base_service import InitBaseService
 from slide_analysis.splitting_service import SplittingService
 
+
 class Model:
     def __init__(self):
         self.descriptors = [HistogramDescriptor, TestDescriptor]
+        self.params = [None, (3, 2, 3)]
 
     def calculate_descriptors(self, idx, filepath, directory_path):
         split = SplittingService()
         stream = split.split_to_tiles(filepath)
 
         init_base = InitBaseService(directory_path)
-        init_base.init_base(stream, HistogramDescriptor, (2, 2, 3), 'image', 'class')
+        init_base.init_base(stream, self.descriptors[idx], self.params[idx], 'image', 'class')
