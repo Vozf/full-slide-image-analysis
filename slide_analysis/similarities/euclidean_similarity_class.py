f672abec9b4015ac6531f40cb18b32604746ede9
tocic <tocic@protonmail.ch>
2017-10-24 22:25:12 +0300

    restructure class, delete examples

--- slide_analysis/similarities/euclidean_similarity_class.py
+++ slide_analysis/similarities/euclidean_similarity_class.py
@@ -2,12 +2,8 @@ import numpy as np
 
 
 class EuclideanSimilarity:
-    @staticmethod
-    def compare(hist1, hist2):
+    def __init__(self):
+        pass
+
+    def compare(self, hist1, hist2):
         return 1 / (1 + np.linalg.norm(hist1 - hist2))
-
-
-if __name__ == '__main__':
-    print(EuclideanSimilarity.compare(np.array([1, 2, 3]), np.array([2, 2, 3])))
-    print(EuclideanSimilarity.compare(np.array([1, 2, 3]), np.array([2, 3, 4])))
-    print(EuclideanSimilarity.compare(np.array([1, 2, 3]), np.array([1, 2, 3])))
