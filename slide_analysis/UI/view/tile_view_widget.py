90b89eb911fe72ceb0d02d4b00d132256a30f685
roskach <sir2805@gmail.com>
2017-10-26 13:36:12 +0300

    Settings dialog window added

--- slide_analysis/UI/view/tile_view_widget.py
+++ slide_analysis/UI/view/tile_view_widget.py
@@ -1,6 +1,5 @@
 from PyQt5 import QtCore
 
-from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QLabel
 
@@ -14,9 +13,7 @@ class TilePreviewPopup(QLabel):
         self.resize(pixmap.width(), pixmap.height())
         position = self.cursor().pos()
         self.move(position)
-        self.setWindowFlags(Qt.Popup | Qt.WindowStaysOnTopHint
-                            | Qt.FramelessWindowHint
-                            | Qt.X11BypassWindowManagerHint)
+
 
     # def leaveEvent(self, event):
     #     """ When the mouse leave this widget, destroy it. """
