2f33cbacb9de6af9d666b56711278f593b82407b
roskach <sir2805@gmail.com>
2017-10-26 16:13:23 +0300

    Settings dialog buttons added(with minor functionality)

--- slide_analysis/UI/view/settings_dialog.py
+++ slide_analysis/UI/view/settings_dialog.py
@@ -8,10 +8,17 @@ class SettingsDialog(QDialog, Ui_SettingsDialog):
     def __init__(self):
         super(QDialog, self).__init__()
         self.setupUi(self)
+        self.init_buttons()
 
     def show_dialog(self):
         self.show()
 
+    def accept(self):
+        self.close()
+
+    def decline(self):
+        self.close()
+
     def init_buttons(self):
         self.settings_conformation_buttons.accepted.connect(self.accept)
-        self.settings_conformation_buttons.rejected.connect(self)
+        self.settings_conformation_buttons.rejected.connect(self.decline)
