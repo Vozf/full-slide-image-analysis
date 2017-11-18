bbd33de84d1e29480f07bba136da58590cfae13c
tocic <tocic@protonmail.ch>
2017-11-18 21:20:21 +0300

    combo boxes are now active

--- slide_analysis/UI/controller/controller_class.py
+++ slide_analysis/UI/controller/controller_class.py
@@ -19,10 +19,16 @@ class Controller:
 
         self.app.installEventFilter(self.image_viewer)
         self.settings = QSettings("grad", "slide_analysis")
-        self.chosen_descriptor_idx = self.settings.value(CHOSEN_DESCRIPTOR_IDX, 1)
-        self.descriptor_params = self.settings.value(DESCRIPTOR_PARAMS, (3, 2, 3))
-        self.chosen_similarity_idx = self.settings.value(CHOSEN_SIMILARITY, 0)
-        self.similarity_params = self.settings.value(SIMILARITY_PARAMS, None)
+        self.chosen_descriptor_idx = self.settings.value(CHOSEN_DESCRIPTOR_IDX,
+                                                         CHOSEN_DESCRIPTOR_IDX_DEFAULT_VALUE,
+                                                         type=int)
+        self.descriptor_params = self.settings.value(DESCRIPTOR_PARAMS,
+                                                     DESCRIPTOR_PARAMS_DEFAULT_VALUE, type=tuple)
+        self.chosen_similarity_idx = self.settings.value(CHOSEN_SIMILARITY_IDX,
+                                                         CHOSEN_SIMILARITY_IDX_DEFAULT_VALUE,
+                                                         type=int)
+        self.similarity_params = self.settings.value(SIMILARITY_PARAMS,
+                                                     SIMILARITY_PARAMS_DEFAULT_VALUE, type=float)
         self.last_descriptor_database = None
         self.selected_dimensions = (BASE_TILE_WIDTH, BASE_TILE_HEIGHT)
 
@@ -33,12 +39,17 @@ class Controller:
         return self.settings.value(CHOSEN_DESCRIPTOR_IDX, CHOSEN_DESCRIPTOR_IDX_DEFAULT_VALUE,
                                    type=int)
 
-    def get_chosen_descriptor_name(self):
-        return self.get_descriptors()[self.get_chosen_descriptor_idx()].__name__
+    def get_descriptor_params(self):
+        return self.settings.value(DESCRIPTOR_PARAMS, DESCRIPTOR_PARAMS_DEFAULT_VALUE,
+                                   type=tuple)
 
-    # def get_fullslide_image_descriptor_directory_path(self):
-    #     filename = self.image_viewer.image_helper.filename
-    #     return filename[0:filename.find('.')].replace('/', ' ')
+    def get_chosen_similarity_idx(self):
+        return self.settings.value(CHOSEN_SIMILARITY_IDX, CHOSEN_SIMILARITY_IDX_DEFAULT_VALUE,
+                                   type=int)
+
+    def get_similarity_params(self):
+        return self.settings.value(SIMILARITY_PARAMS, SIMILARITY_PARAMS_DEFAULT_VALUE,
+                                   type=float)
 
     def run(self):
         self.image_viewer.show()
@@ -53,8 +64,8 @@ class Controller:
 
     def calculate_descriptors(self):
         imagepath = self.get_imagepath()
-        descriptor_base = self.model.calculate_descriptors(self.chosen_descriptor_idx,
-                                                           self.descriptor_params,
+        descriptor_base = self.model.calculate_descriptors(self.get_chosen_descriptor_idx(),
+                                                           self.get_descriptor_params(),
                                                            imagepath, DESCRIPTOR_DIRECTORY_PATH)
         self.last_descriptor_database = descriptor_base
 
@@ -72,7 +83,8 @@ class Controller:
         desc_path = self.last_descriptor_database
         tile = get_tile_from_coordinates(imagepath, *coordinates, *dimensions)
         top_n = self.model.find_similar(desc_path, tile, self.get_chosen_n(),
-                                        self.chosen_similarity_idx, self.similarity_params)
+                                        self.get_chosen_similarity_idx(),
+                                        self.get_similarity_params())
         qts = list(map(lambda tup: self.image_viewer.image_helper.get_qt_from_coordinates(
             tup), top_n))
         self.image_viewer.show_top_n(qts)
